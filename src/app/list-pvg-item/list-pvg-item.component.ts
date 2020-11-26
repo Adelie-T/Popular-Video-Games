5547ebf582bfe8691690522e9b0ab00f8d0397e2
Adelie Thirion <adelie.thirion@gmail.com>
2020-11-26 11:10:51 +0100

    ngFor added for List-PVG-Item

--- src/app/list-pvg-item/list-pvg-item.component.ts
+++ src/app/list-pvg-item/list-pvg-item.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-list-pvg-item',
@@ -9,7 +9,17 @@ export class ListPVGItemComponent implements OnInit {
 
   constructor() { }
 
+  @Input() gameName : string;
+  @Input() releaseDate : Date;
+  @Input() category : string;
+  @Input() ranking : number;
+  @Input() src : string;
+
   ngOnInit(): void {
+  }
+
+  onGetmore() {
+    console.log("afficher seconde page");
   }
 
 }
--- src/app/list-pvg/list-pvg.component.ts
+++ src/app/list-pvg/list-pvg.component.ts
@@ -9,6 +9,22 @@ export class ListPVGComponent implements OnInit {
 
   constructor() { }
 
+  games = [
+    {
+      gameName : "ex1",
+      category : "cat1",
+      ranking : "r1",
+      src : "https://www.jaimelesstartups.fr/wp-content/uploads/2019/12/logo-E2VR.png"
+    },
+    {
+      gameName : "ex2",
+      category : "cat2",
+      ranking : "r2",
+      src : "https://www.jaimelesstartups.fr/wp-content/uploads/2019/12/logo-E2VR.png"
+    }
+
+  ]
+
   ngOnInit(): void {
   }
 
